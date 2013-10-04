cbe9375e7739c81ff47414f15146ee68f514d056
fedor.belov <fedor.belov@mail.ru>
2013-10-05 01:38:35 +0400

    first commit

---
+++ soyuz-app/src/main/java/io/soyuz/app/Start.java
@@ -0,0 +1,13 @@
+/*
+ * Start
+ * Copyright (c) 2012 Cybervision. All rights reserved.
+ */
+package io.soyuz.app;
+
+public class Start {
+
+    public static void main(String[] args) {
+        System.out.println("Hello space!");
+    }
+
+}
---
+++ soyuz-shared/src/main/java/io/soyuz/shared/PluginDescriptor.java
@@ -0,0 +1,8 @@
+/*
+ * PluginDescriptor
+ * Copyright (c) 2012 Cybervision. All rights reserved.
+ */
+package io.soyuz.shared;
+
+public class PluginDescriptor {
+}
